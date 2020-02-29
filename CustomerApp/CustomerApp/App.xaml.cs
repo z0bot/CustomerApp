666e3392a1837b75e85e9af01eee6d7917e0403c
z0bot <z_series@protonmail.ch>
2020-02-28 20:41:04 -0600

    commiting staged from previous master branch build. this is the most up to date branch

--- CustomerApp/CustomerApp/App.xaml.cs
+++ CustomerApp/CustomerApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomerApp.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace CustomerApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Login();
         }
 
         protected override void OnStart()
