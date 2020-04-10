91670805a86979a1489a2444b6252606f1eb292a
z0bot <z_series@protonmail.ch>
2020-04-09 21:04:23 -0500

    Added notifications models and request class. All that's left to do is to tie it to table numbers and employee id's

---
+++ CustomerApp/CustomerApp/Models/NotificationResponse.cs
@@ -0,0 +1,13 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerApp.Models
+{
+    public class NotificationResponse : RealmObject
+    {
+        public string message { get; set; }
+        public Notifications notifications { get; set; }
+    }
+}
---
+++ CustomerApp/CustomerApp/Models/ServiceRequests/PostNotificationsRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CustomerApp.Models.ServiceRequests
+{
+    public class PostNotificationsRequest : ServiceRequest
+    {
+        public override string Url => "https://dijkstras-steakhouse-restapi.herokuapp.com/menuItems";
+        public override HttpMethod Method => HttpMethod.Post;
+        public override Dictionary<string, string> Headers => null;
+        public NotificationRequestBody Body;
+
+        //May need to include employee id
+        public PostNotificationsRequest(string notificationType)
+        {
+            Body = new NotificationRequestBody
+            {
+                notificationType = notificationType,
+            };
+        }
+        public static async Task<bool> SendNotificationRequest(string notificationType)
+        {
+            var sendNotificationRequest = new PostNotificationsRequest(notificationType);
+            var response = await ServiceRequestHandler.MakeServiceCall<NotificationResponse>(sendNotificationRequest, sendNotificationRequest.Body);
+            if(response.message == null)
+            {
+                return false;
+            }
+            else if(response.message == "Employee ID given does not match any ID" )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public class NotificationRequestBody
+        {
+            public string notificationType { get; set; }
+            public string sender { get; set; }
+        }
+    }
+}
