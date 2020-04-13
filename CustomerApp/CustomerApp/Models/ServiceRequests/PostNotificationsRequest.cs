72431e3ce08fc0c6b8605a1f30b93916c3816928
z0bot <z_series@protonmail.ch>
2020-04-12 19:49:33 -0500

    Added refill and help requested notifcations to each page that requires it.

--- CustomerApp/CustomerApp/Models/ServiceRequests/PostNotificationsRequest.cs
+++ CustomerApp/CustomerApp/Models/ServiceRequests/PostNotificationsRequest.cs
@@ -8,22 +8,24 @@ namespace CustomerApp.Models.ServiceRequests
 {
     public class PostNotificationsRequest : ServiceRequest
     {
-         public override string Url => "https://dijkstras-steakhouse-restapi.herokuapp.com/notifications";
+        public override string Url => "https://dijkstras-steakhouse-restapi.herokuapp.com/notifications";
         public override HttpMethod Method => HttpMethod.Post;
         public override Dictionary<string, string> Headers => null;
         public NotificationRequestBody Body;
 
         //May need to include employee id
-        public PostNotificationsRequest(string notificationType)
+        public PostNotificationsRequest(string notificationType, string employeeID, string sender)
         {
             Body = new NotificationRequestBody
             {
                 notificationType = notificationType,
+                employee_id = employeeID,
+                sender = sender
             };
         }
-        public static async Task<bool> SendNotificationRequest(string notificationType)
+        public static async Task<bool> SendNotificationRequest(string notificationType, string employeeID, string sender)
         {
-            var sendNotificationRequest = new PostNotificationsRequest(notificationType);
+            var sendNotificationRequest = new PostNotificationsRequest(notificationType, employeeID, sender);
             var response = await ServiceRequestHandler.MakeServiceCall<NotificationResponse>(sendNotificationRequest, sendNotificationRequest.Body);
             if(response.message == null)
             {
@@ -40,6 +42,7 @@ namespace CustomerApp.Models.ServiceRequests
         }
         public class NotificationRequestBody
         {
+            public string employee_id;
             public string notificationType { get; set; }
             public string sender { get; set; }
         }
