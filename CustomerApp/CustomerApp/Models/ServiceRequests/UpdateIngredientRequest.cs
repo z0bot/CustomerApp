8f293f8ec6e0248fe4475a2b9aa73561105e3e5a
z0bot <z_series@protonmail.ch>
2020-04-18 23:22:35 -0500

    Put ingredients so that when a user sends an order to the server,  all of the ingredients get decremented from the server

---
+++ CustomerApp/CustomerApp/Models/ServiceRequests/UpdateIngredientRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CustomerApp.Models.ServiceRequests
+{
+    class UpdateIngredientRequest : ServiceRequest
+    {
+                public string id;
+        public override string Url => "https://dijkstras-steakhouse-restapi.herokuapp.com/ingredients/" + id;
+        public override HttpMethod Method => HttpMethod.Put;
+        public override Dictionary<string, string> Headers => null;
+        public IList<UpdateIngredientRequestObject> Body;
+
+        public UpdateIngredientRequest(string id, string updateQuantity, string amount)
+        {
+            this.id = id;
+            UpdateIngredientRequestObject updateIngredientRequestObject = new UpdateIngredientRequestObject
+            {
+                propName = updateQuantity,
+                value = amount
+            };
+            Body = new List<UpdateIngredientRequestObject>();
+            Body.Add(updateIngredientRequestObject);
+        }
+        public static async Task<bool> SendUpdateIngredientRequest(string id, string updateQuantity, string amount)
+        {
+            var sendUpdateIngredientRequest = new UpdateIngredientRequest(id, updateQuantity, amount);
+            var response = await ServiceRequestHandler.MakeServiceCall<DeleteResponse>(sendUpdateIngredientRequest, sendUpdateIngredientRequest.Body);
+            if(response == null)
+            {
+                return false;
+            }
+            else
+            { 
+                return true;
+            }
+        }
+    }
+    public class UpdateIngredientRequestObject
+    {
+        public string propName { get; set; }
+        public string value { get; set; }
+    }
+}
